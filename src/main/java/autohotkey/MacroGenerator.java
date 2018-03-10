d00d69869259a833e5b8f41be61ff9a8296ce687
Conrad Weiser <bluecrecent0@gmail.com>
2018-03-10 18:58:29 -0500

    AutoHotkey macro generator class

---
+++ src/main/java/autohotkey/MacroGenerator.java
@@ -0,0 +1,75 @@
+package autohotkey;
+
+
+import systemstudio.elements.FreelancerObjectElement;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.List;
+
+public class MacroGenerator {
+
+    File createMacroSaveLocation;
+    File deleteMacroSaveLocation;
+
+    public MacroGenerator(File createMacroSaveLocation, File deleteMacroSaveLocation) {
+
+        this.createMacroSaveLocation = createMacroSaveLocation;
+        this.deleteMacroSaveLocation = deleteMacroSaveLocation;
+    }
+
+    public void createHotkeyScripts(List<FreelancerObjectElement> availableElements) throws FileNotFoundException {
+
+        //Generate the contents for both deletion, and creation
+        StringBuilder createBuilder = new StringBuilder();
+        StringBuilder deleteBuilder = new StringBuilder();
+
+        //Append the header for the create macro (CTRL+SHIFT+V)
+        createBuilder.append("^+v::\n");
+        createBuilder.append("SEND, {ENTER}\n");
+
+        //Append the header for the delete macro (CTRL+SHIFT+Z)
+        deleteBuilder.append("^+z::\n");
+        deleteBuilder.append("SEND, {ENTER}\n");
+
+        for(FreelancerObjectElement element : availableElements) {
+
+            createBuilder.append("SEND, .custombasecreate ");
+            createBuilder.append(element.getArchtype() + " ");
+            createBuilder.append(element.getLoadout() + " ");
+            createBuilder.append(element.getType() + " ");
+
+            //Position
+            float[] position = element.getPos();
+            createBuilder.append(position[0] + " ").append(position[1] + " ").append(position[2] + " ");
+
+            //Rotation
+            float[] rotation = element.getRot();
+            createBuilder.append(rotation[0] + " ").append(rotation[1] + " ").append(rotation[2] + " ");
+
+            createBuilder.append(element.getAffiliation() + " ");
+            createBuilder.append(element.getName()).append("\n");
+            createBuilder.append("SEND, {ENTER}");
+            createBuilder.append("SEND, {ENTER}");
+
+            //Prepare add the entry for the delete macro
+            deleteBuilder.append("SEND, .basedestroy " + element.getName() + "\n");
+            deleteBuilder.append("SEND, {ENTER}");
+            deleteBuilder.append("SEND, {ENTER}");
+        }
+
+        //Write to the files here
+        PrintWriter createMacroOut = new PrintWriter(createMacroSaveLocation);
+        createMacroOut.println(createBuilder.toString());
+
+        PrintWriter deleteMacroOut = new PrintWriter(deleteMacroSaveLocation);
+        deleteMacroOut.println(deleteBuilder.toString());
+
+        createMacroOut.close();
+        deleteMacroOut.close();
+
+
+    }
+
+}
